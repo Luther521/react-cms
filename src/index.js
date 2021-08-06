f763c6897a56e90f6aa9a0a46e60d29f00a13386
tjl <784939830@qq.com>
2021-08-06 17:27:25 +0800

    初始化项目(备注)

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@
  * @Author: tjl
  * @Date: 2021-08-06 16:42:39
  * @LastEditors: tjl
- * @LastEditTime: 2021-08-06 16:42:51
+ * @LastEditTime: 2021-08-06 17:07:28
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './common/style/frame.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
