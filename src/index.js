f4acea557cef505381dced588f9225e1da84806e
tjl <784939830@qq.com>
2021-08-06 18:04:02 +0800

    修改基本卑职

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
  * @Author: tjl
  * @Date: 2021-08-06 16:42:39
  * @LastEditors: tjl
- * @LastEditTime: 2021-08-06 17:07:28
+ * @LastEditTime: 2021-08-06 18:03:31
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import './common/style/frame.css'
+// import './common/style/frame.less'
 
 ReactDOM.render(<App />, document.getElementById('root'))
